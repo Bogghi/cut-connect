e50b417264f904c0dd14f24ae4fea8a79fc00822
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2025-05-16 09:44:24 +0200

    landing completed

--- front-end/src/landing/main.js
+++ front-end/src/landing/main.js
@@ -1,5 +1,7 @@
 import AppNavigator from '@/shared/components/AppNavBar.vue'
 import '@/shared/assets/css/global.css';
+import '@/shared/assets/css/landing.css'
+import '@/shared/assets/css/footer.css'
 
 import {createApp} from 'vue';
 
